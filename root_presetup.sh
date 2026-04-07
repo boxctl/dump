37e28a10aab99cc8a47b85863297032c794061c5
Tushar <tushar10141@gmail.com>
2026-04-07 11:18:46 +0530

    fixed presetup and added presetup cleanup

--- root_presetup.sh
+++ root_presetup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-{
-    adduser boxadmin
-    usermod -aG sudo boxadmin
-    mkdir -p /home/boxadmin/.ssh
-    cp /root/.ssh/authorized_keys /home/boxadmin/.ssh/authorized_keys
-    chown -R boxadmin:boxadmin /home/boxadmin/.ssh
-    chmod 700 /home/boxadmin/.ssh
-    chmod 600 /home/boxadmin/.ssh/authorized_keys
-}
+
+set -euo pipefail
+
+useradd -m -s /bin/bash -G sudo boxadmin
+mkdir -p /home/boxadmin/.ssh
+cp /root/.ssh/authorized_keys /home/boxadmin/.ssh/authorized_keys
+chown -R boxadmin:boxadmin /home/boxadmin/.ssh
+chmod 700 /home/boxadmin/.ssh
+chmod 600 /home/boxadmin/.ssh/authorized_keys
